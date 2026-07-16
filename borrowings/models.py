c00c317bcb39608dbde0eaeaeb4475504e73d8af
Nik <nikita.pitushkin31@mail.com>
2026-07-16 16:02:17 +0300

    feat: implement model borrowings

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
+from django.db.models import F, Q
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+
+    def __str__(self):
+        return f"{self.borrow_date} - {self.expected_return_date} - {self.actual_return_date}"
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=Q(expected_return_date__gte=F("borrow_date")),
+                name="expected_return_date_gte_borrow_date",
+            ),
+            models.CheckConstraint(
+                condition=Q(actual_return_date__isnull=True) | Q(actual_return_date__gte=F("borrow_date")),
+                name="actual_return_date_isnull_and_gte_borrow_date",
+             )
+        ]
