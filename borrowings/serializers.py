9043ae8e37e3c873fdfd7e200a5be61d4b63c84c
Nik <nikita.pitushkin31@mail.com>
2026-07-16 16:21:02 +0300

    feat: Implement create a serializer & Validate book inventory

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -17,3 +17,19 @@ class BorrowingsSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingsCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "book",
+        )
+
+    def validate_book(self, value):
+        if value.inventory < 1:
+            raise serializers.ValidationError("This book is not available")
+        return value
